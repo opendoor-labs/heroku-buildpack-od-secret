c0af4591de60931eac4fe01187f7d675f2d623f3
Trevor Creech <trevor@opendoor.com>
2020-02-03 16:05:41 -0500

    switch path to env

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -6,6 +6,8 @@ testCompile()
 {
   compile
 
+  assertCapturedSuccess
+
   assertContains "-----> Fetching and vendoring od-secret" "`cat ${STD_OUT}`"
   assertTrue "Should have installed ourselved in build dir: `ls -la ${BUILD_DIR}`" "[ -f ${BUILD_DIR}/vendor/od-secret/od-secret-env ]"
 }
